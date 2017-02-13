e07224b028240df384533ea1d584b7968257ac57
pohan <l.w.jasons@gmail.com>
2017-02-14 02:45:23 +0800

    add basic settings of behave

--- debby/exercise_record/features/ExerciseRecord.feature
+++ debby/exercise_record/features/ExerciseRecord.feature
@@ -5,7 +5,7 @@ Feature: Exercise Record
   身為一個糖尿病患，我希望我可以對chatbot記錄我的運動狀況，因為感覺很方便
 
   Scenario: exercise data-set from GET
-    Given a set of Exercise in Database
+    Given a set of exercise_record.Exercises in Database
     |id|type|start_from|duration|
     |1 |"跑步"|2017-01-21 00:00:00|60|
     |2 |"散步"|2017-01-21 00:00:00|180|
--- debby/exercise_record/features/steps/ExerciseRecord.py
+++ debby/exercise_record/features/steps/ExerciseRecord.py
@@ -1,35 +1,37 @@
+# 需要在 runner.py 299行的 open加入 encoding = 'utf-8-sig' 才能正確顯示中文
+
 from behave import *
+import urllib.request
 
-use_step_matcher("re")
 
-
-@given("a set of Exercise in Database")
-def step_impl(context):
+@given("a set of {model_name}s in Database")
+def step_impl(context, model_name):
     """
     :type context: behave.runner.Context
     """
     pass
 
 
-@when("I go to /exercise/")
-def step_impl(context):
+@when("I go to {page_url}")
+def step_impl(context, page_url):
     """
     :type context: behave.runner.Context
+    """
+    context.response = urllib.request.urlopen(context.test_case.live_server_url + page_url)
+
+
+@then('the return includes "{text}"')
+def step_impl(context, text):
+    """
+    :type context: behave.runner.Context
+    :type text: str
     """
     pass
 
 
-@then('the return includes "跑步"')
-def step_impl(context):
+@step('the return includes "{text}"')
+def step_impl(context, text):
     """
     :type context: behave.runner.Context
     """
-    pass
-
-
-@step('the return includes "散步"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    pass
+    assert False
