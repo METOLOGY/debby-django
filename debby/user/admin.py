c3b1c362a796833b741a4fdb19acb8e9917beb23
Weian Wang <stargazer.wang@gmail.com>
2017-02-15 22:14:33 +0800

    custom user model updated

--- debby/user/admin.py
+++ debby/user/admin.py
@@ -6,4 +6,9 @@ from .models import CustomUserModel
 class CustomUserAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_joined'
     list_display = ('line_id', 'username', 'date_joined')
-    fields = ('line_id', 'username', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('line_id', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ['date_joined']}),
+    )
