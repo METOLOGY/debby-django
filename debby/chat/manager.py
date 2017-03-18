ba4946eca1f9a2584f908f4c3091a8098330b7ac
Weian Wang <stargazer.wang@gmail.com>
2017-03-18 18:29:34 +0800

    update chat manager with action 'READ'

--- debby/chat/manager.py
+++ debby/chat/manager.py
@@ -9,8 +9,9 @@ from line.callback import ChatCallback
 class ChatManager(object):
     chats = []
 
-    def __init__(self, callback: ChatCallback):
+    def __init__(self, callback: ChatCallback, input_text: str):
         self.callback = callback
+        self.input_text = input_text
 
     def is_input_a_chat(self, text: str):
         chats = ChatModel.objects.filter(phrase=text)
@@ -24,3 +25,8 @@ class ChatManager(object):
         chat = random.choice(self.chats)  # type: ChatModel
         message = chat.answer
         return TextSendMessage(text=message)
+
+    def handle(self):
+        if self.callback.action == 'READ':
+            if self.is_input_a_chat(self.input_text):
+                return self.reply_answer()
