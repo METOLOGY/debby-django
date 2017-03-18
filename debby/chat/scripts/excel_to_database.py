a0adac5848366c546283d75ef630f50a9a23926a
Weian Wang <stargazer.wang@gmail.com>
2017-03-18 18:34:58 +0800

    Chat app: excel to database script update

--- debby/chat/scripts/excel_to_database.py
+++ debby/chat/scripts/excel_to_database.py
@@ -1,7 +1,8 @@
 from openpyxl import load_workbook
 from chat.models import ChatModel
+from line.models import EventModel
 
-wb2 = load_workbook('./chat_table/Chatbot Database.xlsx')  # path to .xlsx
+wb2 = load_workbook('../chat_table/Chatbot Database.xlsx')  # path to .xlsx
 chat_table = wb2['問候']  # sheet name
 
 for num, line in enumerate(chat_table.iter_rows()):
@@ -11,7 +12,16 @@ for num, line in enumerate(chat_table.iter_rows()):
             if phrase.value != None:
                 if num > 0:
                     answer = phrase.value
+
+                    # Chat model
                     chat = ChatModel()
                     chat.phrase = question
                     chat.answer = answer
                     chat.save()
+
+        # Event model
+        event = EventModel()
+        event.phrase = question
+        event.callback = 'Chat'
+        event.action = 'READ'
+        event.save()
