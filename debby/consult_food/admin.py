81eae6cd9df951394f8b89099dafe4512a70f399
pohan <l.w.jasons@gmail.com>
2017-03-18 20:27:30 +0800

    add consultFood admin

--- debby/consult_food/admin.py
+++ debby/consult_food/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ConsultFoodModel
+
 
 # Register your models here.
+@admin.register(ConsultFoodModel)
+class ConsultFoodAdmin(admin.ModelAdmin):
+    list_display = ('sample_name', 'modified_calorie',
+                    'carbohydrates', 'dietary_fiber',
+                    'metabolic_carbohydrates', 'carbohydrates_equivalent',
+                    'white_rice_equivalent')
+    fields = ('sample_name', 'modified_calorie',
+              'carbohydrates', 'dietary_fiber',
+              'metabolic_carbohydrates', 'carbohydrates_equivalent',
+              'white_rice_equivalent')
