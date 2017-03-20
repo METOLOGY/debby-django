a5b2a82b724f0284d8cccac7bb6850cf0ea36e27
pohan <l.w.jasons@gmail.com>
2017-03-20 23:26:34 +0800

    fix typo

--- debby/consult_food/manager.py
+++ debby/consult_food/manager.py
@@ -16,11 +16,11 @@ class ConsultFoodManager(object):
         metabolic_carbohydrates = food.metabolic_carbohydrates
         carbohydrates_equivalent = food.carbohydrates_equivalent
         white_rice_equivalent = food.white_rice_equivalent
-        message = "每100克{}含有: 熱量{}大卡，含可代謝醣類{}克，等於{}份醣類，也代表吃了碗白飯".format(sample_name,
-                                                                        modified_calorie,
-                                                                        metabolic_carbohydrates,
-                                                                        carbohydrates_equivalent,
-                                                                        white_rice_equivalent)
+        message = "每100克{}含有: 熱量{}大卡，含可代謝醣類{}克，等於{}份醣類，也代表吃了{}碗白飯".format(sample_name,
+                                                                          modified_calorie,
+                                                                          metabolic_carbohydrates,
+                                                                          carbohydrates_equivalent,
+                                                                          white_rice_equivalent)
         return TextSendMessage(text=message)
 
     def handle(self):
