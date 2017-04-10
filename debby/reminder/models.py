fe20ad72c95587b5b31af4da0b8f3ff7ec2fda4e
weian wang <weian.wang@meridigen.com>
2017-04-10 18:24:46 +0800

    update model

--- debby/reminder/models.py
+++ debby/reminder/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from user.models import CustomUserModel
 from datetime import datetime
 
-# Create your models here.
+
+# reminder number should be limited. 5 reminder for each type, maximum.
 class UserReminder(models.Model):
     user = models.ForeignKey(CustomUserModel)
     time = models.TimeField(default=datetime.time(9, 00))
     status = models.BooleanField(default=True)
-    type = models.CharField(default=True)
+    type = models.CharField(default=True, choices=(
+        ('bg', 'blood sugar'),
+        ('insulin', 'insulin'),
+        ('drug', 'drug')
+    ))
