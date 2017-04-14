cc9f288312d2de1cd09b53ed260ec46533d6eae3
weian wang <weian.wang@meridigen.com>
2017-04-14 11:56:19 +0800

    reminder task

--- debby/reminder/manager.py
+++ debby/reminder/manager.py
@@ -1,19 +1,28 @@
 from django.core.cache import cache
+from django.conf import settings
 from line.callback import ReminderCallback
 from linebot.models import SendMessage
 from linebot.models import TextSendMessage
 from linebot.models import ButtonsTemplate
 from linebot.models import PostbackTemplateAction
 from user.cache import AppCache
+from .models import UserReminder
+
 
 class ReminderManager(object):
     def __init__(self, callback: ReminderCallback):
         self.callback = callback
 
     @staticmethod
-    def reply_reminder(self, type: str, reminder_time='12:00'):
-        reminder_time = 'default reminder time'
-        reminder_text = reminder_time
+    def reply_reminder(line_id: str, type: str):
+        """
+        :param line_id: a true line ID.
+        :param type: reminder type.
+
+        This function is designed for celery beat task.
+        """
+        assert len(line_id) == 33 # check line_id is a true line ID
+
         if type == 'bg':
             reminder_text = 'Debby提醒您: 請記得量血糖喔～'
         elif type == 'insulin':
@@ -22,12 +31,12 @@ class ReminderManager(object):
              reminder_text = 'Debby提醒您：請記得服用藥物哦～'
 
 
-        TextSendMessage(
+        reminder_message = TextSendMessage(
             alt_text='使用者回覆',
             template=ButtonsTemplate(
                 type='buttons',
                 text=reminder_text,
-                title=reminder_time,
+                title=reminder_text,
                 actions=[
                     PostbackTemplateAction(
                         label='好的',
@@ -47,6 +56,10 @@ class ReminderManager(object):
                 ]
             )
         )
+
+        line_bot_api = settings.LINE_BOT_API
+        line_bot_api.push_message(line_id, messages=reminder_message)
+
 
     def reply_reminder_awaits(self):
         return TextSendMessage(text='10分鐘後Debby會再次提醒您量血糖; 您可至"我的設定"中調整提醒時間')
--- debby/reminder/tasks.py
+++ debby/reminder/tasks.py
@@ -1,89 +1,66 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from django_celery_beat.models import PeriodicTask, PeriodicTasks, CrontabSchedule, IntervalSchedule
-from datetime import time
-import json
 
-
-from bg_record.manager import BGRecordManager
-from debby.settings import LINE_BOT_API
 from user.models import CustomUserModel
+from reminder.models import UserReminder
+from reminder.manager import ReminderManager
 
+import json
 
 @shared_task
 def record_bg_reminder(line_id):
     # BGRecordManager.record_reminder(LINE_BOT_API, line_id=line_id)
     print(line_id)
 
+
+@shared_task
+def record_reminder(line_id: str, reminder_type: str):
+    ReminderManager.reply_reminder(line_id=line_id, type=reminder_type)
+
 @shared_task
 def periodic_checking_bg_reminder_setting():
     for user in CustomUserModel.objects.all():
         if len(user.line_id) == 33:
             try:
+                # update the reminder crontab tasks with user reminder model
 
-                interval, _ = IntervalSchedule.objects.get_or_create(every='3', period='seconds')
+                reminders = UserReminder.objects.filter(user=user)
+                for num, reminder in enumerate(reminders):
+                    schedule, _ = CrontabSchedule.objects.get_or_create(
+                        minute=reminder.time.minute,
+                        hour=reminder.time.hour,
+                    )
 
-                # breakfast
-                breakfast_reminder = user.usersettingmodel_set.last().breakfast_reminder # TYPE: datatime.time
-                breakfast_schedule, _ = CrontabSchedule.objects.get_or_create(
-                    minute=breakfast_reminder.minute,
-                    hour=breakfast_reminder.hour
-                )
-                breakfast, _ = PeriodicTask.objects.get_or_create(
-                    name=user.line_id + '_breakfast',
-                )
-
-                # if time changed, than recreat
-                if breakfast.crontab != breakfast_schedule or breakfast.crontab == None:
-                    # breakfast.crontab = breakfast_schedule
-                    breakfast.interval = interval
-                    breakfast.task = 'line.tasks.record_bg_reminder'
-                    breakfast.args = json.dumps([user.line_id])
-                    breakfast.enabled = True
-                    breakfast.save()
-                    print('breakfast changed')
-                    PeriodicTasks.changed(breakfast)
+                    reminder_task, _ = PeriodicTask.objects.get_or_create(
+                        name=user.line_id + num,
+                        task='line.tasks.record_reminder',
+                        args=json.dumps([user.line_id, reminder.type]),
+                        enable=True
+                    )
 
 
-                # lunch
-                lunch_reminder = user.usersettingmodel_set.last().lunch_reminder
-                lunch_schedule, _ = CrontabSchedule.objects.get_or_create(
-                    minute=lunch_reminder.minute,
-                    hour=lunch_reminder.hour
-                )
 
-                lunch, _ = PeriodicTask.objects.get_or_create(
-                    name=user.line_id + '_lunch',
-                )
+                # breakfast_reminder = user.usersettingmodel_set.last().breakfast_reminder # TYPE: datatime.time
+                # breakfast_schedule, _ = CrontabSchedule.objects.get_or_create(
+                #     minute=breakfast_reminder.minute,
+                #     hour=breakfast_reminder.hour
+                # )
+                # breakfast, _ = PeriodicTask.objects.get_or_create(
+                #     name=user.line_id + '_breakfast',
+                # )
+                #
+                # # if time changed, than recreat
+                # if breakfast.crontab != breakfast_schedule or breakfast.crontab == None:
+                #     breakfast.crontab = breakfast_schedule
+                #     # breakfast.interval = interval
+                #     breakfast.task = 'line.tasks.record_bg_reminder'
+                #     breakfast.args = json.dumps([user.line_id])
+                #     breakfast.enabled = True
+                #     breakfast.save()
+                #     print('breakfast changed')
+                #     PeriodicTasks.changed(breakfast)
 
-                if lunch.crontab != lunch_schedule or lunch.crontab == None:
-                    lunch.crontab = lunch_schedule
-                    lunch.task = 'line.tasks.record_bg_reminder'
-                    lunch.args = json.dumps([user.line_id])
-                    lunch.enabled = True
-                    lunch.save()
-                    print('lunch changed')
-                    PeriodicTasks.changed(lunch)
-
-
-                # dinner
-                dinner_reminder = user.usersettingmodel_set.last().dinner_reminder
-                dinner_schedule, _ = CrontabSchedule.objects.get_or_create(
-                    minute=dinner_reminder.minute,
-                    hour=dinner_reminder.hour
-                )
-
-                dinner, _ = PeriodicTask.objects.get_or_create(
-                    name=user.line_id + '_dinner',
-                )
-                if dinner.crontab != dinner_schedule or dinner.crontab == None:
-                    dinner.crontab = dinner_schedule
-                    dinner.task = 'line.tasks.record_bg_reminder'
-                    dinner.args = json.dumps([user.line_id])
-                    dinner.enabled = True
-                    dinner.save()
-                    print('dinner changed')
-                    PeriodicTasks.changed(dinner)
 
                 # PeriodicTasks.changed()
 
