0626efed70fb7e289bdc75406ca516af0d539db9
weian wang <weian.wang@meridigen.com>
2017-04-17 10:35:50 +0800

    add event model script

--- debby/line/scripts/save_callback_to_model.py
+++ debby/line/scripts/save_callback_to_model.py
@@ -4,3 +4,8 @@ EventModel.objects.get_or_create(phrase='記錄飲食', callback='FoodRecord', a
 EventModel.objects.get_or_create(phrase='記錄血糖', callback='BGRecord', action='CREATE')
 EventModel.objects.get_or_create(phrase='食物查詢', callback='FoodQuery', action='READ')
 EventModel.objects.get_or_create(phrase='藥物查詢', callback='DrugQuery', action='READ')
+
+EventModel.objects.get_or_create(phrase='血糖紀錄', callback='BGRecord', action='CREATE_FROM_MENU')
+EventModel.objects.get_or_create(phrase='飲食紀錄', callback='FoodRecord', action='CREATE_FROM_MENU')
+EventModel.objects.get_or_create(phrase='食物熱量查詢', callback='FoodQuery', action='READ_FROM_MENU')
+EventModel.objects.get_or_create(phrase='藥物資訊查詢', callback='DrugQuery', action='READ_FROM_MENU')
