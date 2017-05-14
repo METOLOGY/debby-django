b534627ea9ee330fc95df864c1a6858be9e3302c
Weian Wang <stargazer.wang@gmail.com>
2017-05-15 01:26:22 +0800

    not properly work

--- debby/reminder/tasks.py
+++ debby/reminder/tasks.py
@@ -1,85 +1,60 @@
 from __future__ import absolute_import, unicode_literals
 
 import json
-
-#from celery import shared_task
-#from django_celery_beat.models import PeriodicTask, CrontabSchedule
-
+import arrow
+import pytz
 from django_q.models import Schedule
 
 from reminder.manager import ReminderManager
 from reminder.models import UserReminder
 from user.models import CustomUserModel
 
-def record_reminder(line_id: str, reminder_id: int, **kwargs):
-    line_id = kwargs['line_id']
-    reminder_id = kwargs['reminder_id']
-    ReminderManager.reply_reminder(line_id=line_id, reminder_id=reminder_id)
+tz = pytz.timezone('Asia/Taipei')
+
+
+def record_reminder(data):
+    line_id = data[0]
+    reminder_id = data[1]
+    reminder = UserReminder.objects.get(id=reminder_id)
+    if reminder.status:
+        ReminderManager.reply_reminder(line_id=line_id, reminder_id=reminder_id)
 
 
 def periodic_checking_bg_reminder_setting():
     for user in CustomUserModel.objects.all():
         if len(user.line_id) == 33:
-            # try:
-                # update the reminder crontab tasks with user reminder model
-
             reminders = UserReminder.objects.filter(user=user)
+
             for num, reminder in enumerate(reminders):
-
-
 
                 sch, _ = Schedule.objects.get_or_create(
                     name=user.line_id + '_reminder_' + str(num),
                 )
 
-                sch.task = 'reminder.task.record_reminder'
-                sch.args = {
-                    'line_id': user.line_id,
-                    'reminder_id': 'reminder.id'
-                }
+                sch_ori_time = sch.next_run.astimezone(tz=tz)
 
-                sch.next_run = reminder.time
-                sch.schedule_type = Schedule.DAILY
-                sch.save()
+                if sch.repeats == 1 or sch.repeats == -1:
+                    sch_status = True
+                else:
+                    sch_status = False
 
-                    # schedule, _ = CrontabSchedule.objects.get_or_create(
-                    #     minute=reminder.time.minute,
-                    #     hour=reminder.time.hour,
-                    # )
-                    #
-                    # reminder_task, _ = PeriodicTask.objects.get_or_create(
-                    #     name=user.line_id + '_reminder_' + str(num),
-                    # )
-                    #
-                    # reminder_task.crontab = schedule
-                    # reminder_task.task = 'reminder.tasks.record_reminder'
-                    # reminder_task.args = json.dumps([user.line_id, reminder.id])
-                    # reminder_task.enabled = True
-                    # reminder_task.save()
+                if (sch_ori_time.hour != reminder.time.hour and sch_ori_time.minute != reminder.time.minute) or sch_status != reminder.status:
+                    print(sch_ori_time)
+                    print(reminder.time)
+                    print((sch_ori_time.hour != reminder.time.hour and sch_ori_time.minute != reminder.time.minute))
+                    print(sch_status != reminder.status)
+                    sch.func = 'reminder.tasks.record_reminder'
+                    sch.args = json.dumps([user.line_id, reminder.id])
 
+                    if reminder.status:
+                        sch.repeats = 1  # always turn on
+                    else:
+                        sch.repeats = 0  # turn off
 
-                    # breakfast_reminder = user.usersettingmodel_set.last().breakfast_reminder # TYPE: datatime.time
-                    # breakfast_schedule, _ = CrontabSchedule.objects.get_or_create(
-                    #     minute=breakfast_reminder.minute,
-                    #     hour=breakfast_reminder.hour
-                    # )
-                    # breakfast, _ = PeriodicTask.objects.get_or_create(
-                    #     name=user.line_id + '_breakfast',
-                    # )
-                    #
-                    # # if time changed, than recreat
-                    # if breakfast.crontab != breakfast_schedule or breakfast.crontab == None:
-                    #     breakfast.crontab = breakfast_schedule
-                    #     # breakfast.interval = interval
-                    #     breakfast.task = 'line.tasks.record_bg_reminder'
-                    #     breakfast.args = json.dumps([user.line_id])
-                    #     breakfast.enabled = True
-                    #     breakfast.save()
-                    #     print('breakfast changed')
-                    #     PeriodicTasks.changed(breakfast)
-
-
-                    # PeriodicTasks.changed()
-
-            # except PeriodicTask.DoesNotExist as e:
-            #     print(e)
+                    sch_time = arrow.utcnow().to('Asia/Taipei').replace(
+                        hour=reminder.time.hour, minute=reminder.time.minute
+                    )
+                    sch.next_run = str(sch_time)
+                    sch.schedule_type = Schedule.DAILY
+                    print('save')
+                    sch.save()
