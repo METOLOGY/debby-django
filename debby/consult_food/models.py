ac44d37e48abbdc27035551ab9fd908ea1dd4429
pohan <l.w.jasons@gmail.com>
2017-05-15 22:54:14 +0800

    MIGRATE: add fields in FoodModel

--- debby/consult_food/models.py
+++ debby/consult_food/models.py
@@ -13,11 +13,15 @@ class ConsultFoodModel(models.Model):
 
 
 class FoodModel(models.Model):
+    integration_number = models.CharField(verbose_name="整合編號", max_length=20)
+    food_type = models.CharField(verbose_name="食物分類", max_length=20)
     sample_name = models.CharField(verbose_name="樣品名稱", max_length=100, unique=True)
     modified_calorie = models.FloatField(verbose_name="修正熱量-成分值(kcal)")
     crude_protein = models.FloatField(verbose_name="粗蛋白-成分值(g)")
     crude_fat = models.FloatField(verbose_name="粗脂肪-成分值(g)")
-    metabolic_carbohydrates = models.FloatField(verbose_name="可代謝醣量")
+    carbohydrates = models.FloatField(verbose_name="總碳水化合物-成分值(g)", blank=True, null=True)
+    dietary_fiber = models.FloatField(verbose_name="膳食纖維-成分值(g)", blank=True, null=True)
+    metabolic_carbohydrates = models.FloatField(verbose_name="可代謝醣類(g)")
 
 
 class FoodNameModel(models.Model):
