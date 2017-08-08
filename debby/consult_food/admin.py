8eb4d58c2aff1ca17b4c97515307f28f7fd5c411
pohan <l.w.jasons@gmail.com>
2017-08-08 14:01:25 +0800

    delete old model

--- debby/consult_food/admin.py
+++ debby/consult_food/admin.py
@@ -1,19 +1,6 @@
 from django.contrib import admin
 
-from .models import ConsultFoodModel, TaiwanSnackModel
-
-
-# Register your models here.
-@admin.register(ConsultFoodModel)
-class ConsultFoodAdmin(admin.ModelAdmin):
-    list_display = ('sample_name', 'modified_calorie',
-                    'carbohydrates', 'dietary_fiber',
-                    'metabolic_carbohydrates', 'carbohydrates_equivalent',
-                    'white_rice_equivalent')
-    fields = ('sample_name', 'modified_calorie',
-              'carbohydrates', 'dietary_fiber',
-              'metabolic_carbohydrates', 'carbohydrates_equivalent',
-              'white_rice_equivalent')
+from .models import TaiwanSnackModel
 
 
 @admin.register(TaiwanSnackModel)
