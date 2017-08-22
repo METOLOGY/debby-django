6dc50c3a2e5b97c78fd34768821f715f7884826a
pohan <l.w.jasons@gmail.com>
2017-08-22 16:14:05 +0800

    implement new img recog flowchart
    merge conflicts

--- debby/debby/utils.py
+++ debby/debby/utils.py
@@ -4,6 +4,10 @@ import math
 from typing import List, Union
 
 from django.core.cache import cache
+from linebot.models import ImageSendMessage, TextSendMessage
+
+from consult_food.models import NutritionModel, SynonymModel, FoodModel, TaiwanSnackModel, ICookIngredientModel, \
+    ICookDishModel
 
 
 def get_each_card_num(choice_num: int) -> List[int]:
@@ -39,3 +43,44 @@ def is_future_mode_on(line_id: str):
 
 def is_demo_mode_on(line_id: str):
     return cache.get(line_id + '_demo')
+
+
+def reply_nutrition(count_word: str, nutrition: NutritionModel) -> List[Union[TextSendMessage, ImageSendMessage]]:
+    reply = list()
+
+    text_message = TextSendMessage(text=count_word)
+    reply.append(text_message)
+
+    url = nutrition.nutrition_amount_image.url
+    preview_url = nutrition.nutrition_amount_image_preview.url
+
+    host = cache.get("host_name")
+    photo = "https://{}{}".format(host, url)
+    preview_photo = "https://{}{}".format(host, preview_url)
+    calories = ImageSendMessage(original_content_url=photo,
+                                preview_image_url=preview_photo)
+    reply.append(calories)
+
+    if nutrition.is_six_group_valid():
+        url = nutrition.six_group_portion_image.url
+        preview_url = nutrition.six_group_portion_image_preview.url
+
+        host = cache.get("host_name")
+        photo = "https://{}{}".format(host, url)
+        preview_photo = "https://{}{}".format(host, preview_url)
+        six_group = ImageSendMessage(original_content_url=photo,
+                                     preview_image_url=preview_photo)
+        reply.append(six_group)
+
+    return reply
+
+
+def get_count_word(content_model: object):
+    if isinstance(content_model, FoodModel):
+        return "每{}克".format(int(content_model.nutrition.gram))
+    elif isinstance(content_model, TaiwanSnackModel):
+        return "每{}".format(content_model.count_word)
+    elif isinstance(content_model, ICookIngredientModel):
+        return "每{}克".format(int(content_model.nutrition.gram))
+    elif isinstance(content_model, ICookDishModel):
+        return "每{}".format(content_model.count_word)
