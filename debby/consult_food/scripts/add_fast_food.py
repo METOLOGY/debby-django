a8a0b8484eba1379f4fcd8ce15af28b466812070
pohan <l.w.jasons@gmail.com>
2017-08-26 01:33:44 +0800

    update script

--- debby/consult_food/scripts/add_fast_food.py
+++ debby/consult_food/scripts/add_fast_food.py
@@ -11,18 +11,20 @@ django.setup()
 
 def run():
     from consult_food.models import FoodModel, NutritionModel, NutritionTuple
+    from debby import utils
     chat_table_dir = os.path.join(settings.PROJECT_DIR, 'chat_table')
     file_path = os.path.join(chat_table_dir, 'fast_food.csv')
 
     with open(file_path, encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
+            print(row['name'])
             nutrition = NutritionTuple(name=row['name'],
-                                       gram=row['gram'],
-                                       calories=row['calories'],
-                                       protein=row['protein'],
-                                       fat=row['fat'],
-                                       carbohydrates=row['carbohydrates'])
+                                       gram=utils.to_number(row['gram']),
+                                       calories=utils.to_number(row['calories']),
+                                       protein=utils.to_number(row['protein']),
+                                       fat=utils.to_number(row['fat']),
+                                       carbohydrates=utils.to_number(row['carbohydrates']))
             nutrition_model = NutritionModel.objects.create(**nutrition._asdict())
             nutrition_model.make_and_save_calories_image()
 
